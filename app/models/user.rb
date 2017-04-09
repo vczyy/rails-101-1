e542f7fab9a57708eb0e7d763d01b968926dd4bd
cili <531860873@qq.com>
2017-04-09 18:33:00 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+has_many :groups
+has_many :posts
 has_many :group_relationships
 has_many :members, through: :group_relationships, source: :user
 
 def is_member_of?(group)
       participated_groups.include?(group)
     end
+
+    def join!(group)
+    participated_groups << group
+  end
+
+  def quit!(group)
+    participated_groups.delete(group)
+  end
+  
 end
